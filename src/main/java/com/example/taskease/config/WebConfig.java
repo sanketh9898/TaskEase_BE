0e7cd5223b886604b161d2de033e2211c78d35e5
sanketh9898 <49584673+sanketh9898@users.noreply.github.com>
2025-04-08 00:08:23 -0400

    Update WebConfig.java

--- src/main/java/com/example/taskease/config/WebConfig.java
+++ src/main/java/com/example/taskease/config/WebConfig.java
@@ -10,10 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/api/**") // Apply to all API endpoints under /api
-                    .allowedOrigins("*") // Allow all origins (FOR DEVELOPMENT ONLY!)
+                   // .allowedOrigins("*") // Allow all origins (FOR DEVELOPMENT ONLY!)
+                    .allowedOrigins("http://35.170.182.117")
+
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed methods
                     .allowedHeaders("*") // Allowed headers
-                    .allowCredentials(false) // Set to true if cookies/credentials needed
+                    .allowCredentials(true) // Set to true if cookies/credentials needed
                     .maxAge(3600); // Cache preflight response (seconds)
         }
 }
